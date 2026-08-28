482935c36e515bd94ea6ff75a76161b57ec50844
Teejay <taofeekson20@email.com>
2026-08-28 02:33:15 +0100

    0hj

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ if (form) {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    if (username === "Angeleyes1983" && password === "Baxter1983") {
+    if (username === "Angeleyes1983" && password === "Baxter19831") {
       localStorage.setItem("isLoggedIn", "true");
       localStorage.setItem("username", username);
 
